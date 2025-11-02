6f73bcdca674f791e13c0edc3ccd423ee6aa551d
maxim <maximchukavovmc@gmail.com>
2025-11-02 16:14:38 +0300

    run settings

--- src/main.py
+++ src/main.py
@@ -4,6 +4,8 @@ from fastapi import FastAPI
 app = FastAPI()
 
 if __name__ == '__main__':
-    uvicorn.run("main:app", reload=True)
-
-print("gir")
+    uvicorn.run("main:app",
+                host=settings.run.host,
+                port=settings.run.port,
+                reload=True,
+    )
